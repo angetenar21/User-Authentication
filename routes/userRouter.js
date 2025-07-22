ceae0e56650a9c8356de79ada940de71b040de5a
Manish Yadav <supermmanish@gmail.com>
2025-07-22 16:32:40 +0530

    Update userRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const taskController = require('../controllers/authController');
+const taskController = require('../controllers/taskController');
 
 //regiuster routes
 router.get('/register', authController.getRegister);
